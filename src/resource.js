05a9e1f5efe0a91b9ef4fb2359819a71623c081f
weiwei.huang <18686672327@163.com>
2016-02-28 22:20:35 +0800

    first add all file

---
+++ src/resource.js
@@ -0,0 +1,22 @@
+var s_HelloWorld = "HelloWorld.jpg";
+var s_CloseNormal = "CloseNormal.png";
+var s_CloseSelected = "CloseSelected.png";
+var s_Plane = "plane.png";
+
+var g_resources = [
+    //image
+    s_HelloWorld,
+    s_CloseNormal,
+    s_CloseSelected,
+    s_Plane
+
+    //plist
+
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
